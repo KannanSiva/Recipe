410cbbbdb1673272ab78e4d52c4039631372865d
Makankan <matadorsmca@gmail.com>
2021-08-31 13:59:06 +0530

    Adding Jpa Entity

---
+++ src/main/java/com/example/domain/Difficulty.java
@@ -0,0 +1,7 @@
+package com.example.domain;
+
+public enum Difficulty {
+	
+	EASY,MODERATE,KIND_OF_HARD,HARD
+
+}
--- src/main/java/com/example/domain/Recipe.java
+++ src/main/java/com/example/domain/Recipe.java
@@ -1,13 +1,16 @@
-package domain;
+package com.example.domain;
 
 import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -31,10 +34,15 @@ public class Recipe {
 	
 	@Lob
 	private Byte[] image;
-	//private Difficulty difficulty;
+	
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
 	
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
+	
+	@ManyToMany
+	private Set<Category> category;
 	
 	
 	public Long getId() {
@@ -96,6 +104,24 @@ public class Recipe {
 	}
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
+	public Set<Category> getCategory() {
+		return category;
+	}
+	public void setCategory(Set<Category> category) {
+		this.category = category;
 	}
 	
 	
